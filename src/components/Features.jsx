7bbdea7a63bed713a5fa9078073e25e4f67543ba
RIZKY <rzkylimitx@gmail.com>
2025-02-11 10:05:22 +0700

    test

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -90,7 +90,7 @@ export const BentoCard = ({ src, title, description, isComingSoon }) => {
                             }}
                         />
                         <TiLocationArrow className="relative z-20" />
-                        <p className="relative z-20">coming soon</p>
+                        <p className="relative z-20">click here</p>
                     </div>
                 )}
             </div>
